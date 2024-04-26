3c3086b619aab7ee18fbe0dc2d3131060554fe36
mzxjhx <mzx0334@163.com>
2024-04-26 15:42:58 +0800

    增加图片，增加肺系统病的一些方剂

---
+++ js/共有的.js
@@ -0,0 +1,81 @@
+                            {
+                                name:'',
+                                key:'',
+                                gong:'',
+                                zhi:[
+                                    ''
+                                ],
+                                fangjie:[
+                                    {
+                                        name:'',
+                                        number:9,
+                                        role:1,
+                                        fun:'',
+                                        img:'中药图片/'
+                                    }
+                                ]
+                            },
+							
+							
+							
+									{
+                                        name:'',
+                                        number:9,
+                                        role:'',
+                                        fun:'',
+                                        img:'中药图片/'
+                                    },
+									
+									
+									{
+                                        name:'半夏',
+                                        number:15,
+                                        role:'君',
+                                        qi:1,
+                                        fun:'燥湿化痰，降逆止呕',
+                                        img:'./中药图片/温化寒痰/半夏.jpeg'
+                                    },
+									{
+                                        name:'桔梗',
+                                        number:6,
+                                        role:'',
+                                        fun:'载药上行，宣肺袪痰',
+                                        img:'中药图片/清化热痰/桔梗.jpg'
+                                    },
+									{
+                                        name:'枳壳',
+                                        number:6,
+                                        role:'',
+                                        qi:-1,
+                                        fun:'破气消积化痰',
+                                        img:'中药图片/理气药/枳实.jpg'
+                                    },
+									{
+                                        name:'茯苓',
+                                        number:12,
+                                        role:'君',
+                                        fun:'健脾利水渗湿，杜生痰之源',
+                                        img:'./中药图片/利水渗湿/利水消肿/茯苓.jpg'
+                                    },
+									{
+                                        name:'陈皮',
+                                        number:6,
+                                        role:'',
+                                        fun:'理气健脾，燥湿化痰',
+                                        img:'中药图片/理气药/陈皮.jpg'
+                                    },
+									{
+                                        name:'灸甘草',
+                                        number:6,
+                                        role:'使',
+                                        fun:'调和',
+                                        img:'中药图片/补气/灸甘草.jpg'
+                                    },
+									
+									
+									
+									
+									
+									
+									
+									
